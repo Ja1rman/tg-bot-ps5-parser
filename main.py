b56c5600d92274e906cc3c9c0511a3ae5a994cb5
Ja1rman <49319500+Ja1rman@users.noreply.github.com>
2021-02-18 19:55:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,21 +12,18 @@ CHANNEL = '@ps5parser'
 proxies = { 'https' : 'https://MiSyCcnd:qVgHXfYS@45.138.147.177:53094' } 
 
 ozonUrls = ["https://www.ozon.ru/context/detail/id/207702519/",
-        "https://www.ozon.ru/context/detail/id/207702520/", 
-        "https://www.ozon.ru/context/detail/id/216940493/",
-        "https://www.ozon.ru/context/detail/id/178337786/",
-        "https://www.ozon.ru/context/detail/id/173667655/",
-        "https://www.ozon.ru/context/detail/id/178715781"]
+            "https://www.ozon.ru/context/detail/id/207702520/", 
+            "https://www.ozon.ru/context/detail/id/178337786/",
+            "https://www.ozon.ru/context/detail/id/178715781/"]
 
 def ozon(url):
     while True:
         try:
-            headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0"} 
+            headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0"} 
             response = requests.get(url, headers=headers)
             r = response.text
             status = r[r.find('isAvailable')+13:]
             status = status[:status.find(',')]
-            print(response.status_code)
             if status == 'true': bot.send_message(CHANNEL, url, disable_web_page_preview=True)
         except: print(traceback.format_exc())
 
@@ -112,7 +109,7 @@ if __name__ == "__main__":
     for i in range(len(ozonUrls)):
         threads.append(mp.Process(target=ozon, args=(ozonUrls[i],)))
         threads[-1].start()
-    
+    '''
     for i in range(len(wildberriesUrls)):
         threads.append(mp.Process(target=wildberries, args=(wildberriesUrls[i],)))
         threads[-1].start()
@@ -134,4 +131,5 @@ if __name__ == "__main__":
         threads[-1].start()
 
     threads.append(mp.Process(target=sony, args=('https://store.sony.ru/common/ajax_product.php?action=refresh_product_state&p_ids=[317406,317400]',)))
-    threads[-1].start()
+    threads[-1].start()'''
+    
