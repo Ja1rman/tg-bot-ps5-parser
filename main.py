0003fe66b12920c97e3d9ce08f9eca88eec2a54e
Ja1rman <49319500+Ja1rman@users.noreply.github.com>
2021-02-20 20:54:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,15 @@ bot = telebot.TeleBot('1680508706:AAGu_zrjj1X9BzYMNUhb3CW1E7ABey4Ft8Q')
 CHANNEL = '@ps5parser'
 
 proxies = ["https://MiSyCcnd:qVgHXfYS@45.138.147.177:53094",
+           "https://MiSyCcnd:qVgHXfYS@45.146.168.63:50604",
+           "https://MiSyCcnd:qVgHXfYS@45.139.52.20:61733",
+           "https://MiSyCcnd:qVgHXfYS@94.154.191.244:59947"]
+
+'''
            "https://MiSyCcnd:qVgHXfYS@46.150.247.144:47822",
            "https://MiSyCcnd:qVgHXfYS@194.156.104.17:58498",
            "https://MiSyCcnd:qVgHXfYS@91.191.184.107:63847"]
+'''
 
 ozonUrls = ["https://www.ozon.ru/context/detail/id/207702519/",
             "https://www.ozon.ru/context/detail/id/207702520/", 
@@ -24,7 +30,7 @@ def ozon(url, proxie):
         try:
             headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0"} 
             session = requests.Session()
-            response = session.get(url, headers=headers, proxies={'https' : proxie}, verify=False)
+            response = session.get(url, headers=headers, proxies={'https' : proxie})
             r = response.text
             status = r[r.find('isAvailable')+13:]
             status = status[:status.find(',')]
@@ -90,7 +96,7 @@ def c1(url, stat):
     while True:
         try:
             headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0"} 
-            if stat == 1: response = requests.get(url, headers=headers, proxies={'https': proxies[0]}, verify=False)
+            if stat == 1: response = requests.get(url, headers=headers, proxies={'https': proxies[0]})
             else: response = requests.get(url, headers=headers)
             r = response.text            
             if 'Перейти в корзину' in r: bot.send_message(CHANNEL, url, disable_web_page_preview=True)
